0dc396e1fca057d0f5a937255ac581d419969950
Alok Pandit <alokpandit@outlook.com>
2021-12-11 15:40:43 +0530

    .

--- init_coturn.sh
+++ init_coturn.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+internalIp="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
+echo "listening-port=3478
+tls-listening-port=5349
+listening-ip="$internalIp"
+relay-ip="$internalIp"
+external-ip="$externalIp"
+no-stdout-log"  | tee /etc/turnserver.conf
+
+turnadmin -a
 
 echo "Starting TURN/STUN server"
 
