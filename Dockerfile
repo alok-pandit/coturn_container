36a6feb492733ea90d9bafc3a99b801e9dff0b08
alok-pandit <alokpandit@outlook.com>
2021-12-13 21:10:02 +0530

    .

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ ENV TURN_PORT_START 10000
 ENV TURN_PORT_END 11000
 ENV TURN_SECRET mysecret
 ENV TURN_SERVER_NAME coturn
-# ENV TURN_REALM com.2waay
+ENV TURN_REALM com.2waay
 # -d -p 3478:3478 -p 3478:3478/udp -p 10000-11000:10000-11000/udp --network=host
 ADD init_coturn.sh init_coturn.sh
 RUN chmod +x init_coturn.sh
--- init_coturn.sh
+++ init_coturn.sh
@@ -5,11 +5,26 @@ externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 echo "listening-port=3478
 listening-ip="$internalIp"
 relay-ip="$internalIp"
-external-ip="$externalIp"
+external-ip="$externalIp/$internalIp"
+stale-nonce=600
+no-sslv2
+no-sslv3
+no-tlsv1
+no-tlsv1_1
+no-tlsv1_2
+# proc-group=mobylogic
+# proc-user=mobylogic
+mobility
+max-allocate-timeout=180
+no-dtls
+no-tls
+user=2waay:2waay@1234
+verbose
+user="":""
 no-stdout-log"  | tee /etc/turnserver.conf
 
 turnadmin -a
 
 echo "Starting TURN/STUN server"
 
-turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --static-auth-secret="${TURN_SECRET}" -p ${TURN_PORT} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END} ${TURN_EXTRA}
+turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --realm=${TURN_REALM} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END}
