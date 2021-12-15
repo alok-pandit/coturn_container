89af3d16a3e8d3657a43ccce1827a403223ae322
Alok Pandit <alokpandit@outlook.com>
2021-12-15 16:40:48 +0530

    Update init_coturn.sh

--- init_coturn.sh
+++ init_coturn.sh
@@ -30,4 +30,4 @@ Verbose"  | tee /etc/turnserver.conf
 
 echo "Starting TURN/STUN server"
 
-turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --realm=${TURN_REALM} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END}
+sudo turnserver -a -v -L 0.0.0.0 --server-name "${TURN_SERVER_NAME}" --realm=${TURN_REALM} --min-port ${TURN_PORT_START} --max-port ${TURN_PORT_END}
